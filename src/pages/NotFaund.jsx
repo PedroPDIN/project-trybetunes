fa35d5b1a7215550906ad30f026b0ec1f983d572
Pedro <pedro.oficial1969@gmail.com>
2021-11-11 16:39:55 -0300

    adiciona requisito 1 concluido

---
+++ src/pages/NotFaund.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">foud</div>
+    );
+  }
+}
+
+export default NotFound;

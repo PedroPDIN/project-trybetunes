a616d4379efedcc856ca29bce7205141036201e6
Pedro <pedro.oficial1969@gmail.com>
2021-11-12 20:18:50 -0300

    adiciona requisito 5 concluido

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,58 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.onInputChange = this.onInputChange.bind(this);
+
+    this.state = {
+      isDisabled: true,
+      inputSearch: '',
+    };
+  }
+
+  onInputChange({ target }) {
+    const { value } = target;
+    this.setState({ inputSearch: value }, this.checkButton);
+  }
+
+  checkButton() {
+    const { inputSearch } = this.state;
+    const TWO = 2;
+    if (inputSearch.length >= TWO) {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
+  }
+
   render() {
+    const { isDisabled, inputSearch } = this.state;
     return (
       <div>
         <Header />
-        <div data-testid="page-search">search</div>
+        <div data-testid="page-search">
+          <form action="">
+
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              name="inputSearch"
+              value={ inputSearch }
+              onChange={ this.onInputChange }
+            />
+
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ isDisabled }
+            >
+              Pesquisa
+            </button>
+
+          </form>
+        </div>
       </div>
 
     );
